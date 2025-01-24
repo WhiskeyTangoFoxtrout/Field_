d425e31bae50dae9a662729ab5f7834874352423
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-24 15:17:09 -0600

    Create Rails.py

---
+++ Rails.py
@@ -0,0 +1,7 @@
+import time,sys
+
+def delay_print(z)://self elxplain wanna put in go
+    for t in z:#this goes in helper funcs
+        sys.stdout.write(t)//std.io
+        sys.stdout.flush()//std.out i think 
+        time.sleep(.05)//pause btw char r std.output 
