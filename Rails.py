f41c99a325725396ab9403d0f3bbba60987cfad4
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-25 10:49:22 -0600

    Update Rails.py

--- Rails.py
+++ Rails.py
@@ -1,13 +1,23 @@
-import time,sys
+#************************************************************************************************************************
 
+                                      ;This haven't been compiled take it as you want;
+
+#*************************************************************************************************************************
+
+import time,sys
+#this func has been proved
 def delay_print(z)://self elxplain wanna put in go
     for t in z:#this goes in helper funcs
         sys.stdout.write(t)//std.io
         sys.stdout.flush()//std.out i think 
         time.sleep(.05)//pause btw char r std.output 
 
-#try and think of a copy func right here
 
+
+
+
+#try and think of a copy func right here
+#EXP.
 class cpy:
 
     def __init__(self):#gotta remember this is the args
