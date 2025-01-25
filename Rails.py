cff0c47d451616c72528454f8df3b86bb68f61a0
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-25 10:46:06 -0600

    Update Rails.py

--- Rails.py
+++ Rails.py
@@ -5,3 +5,18 @@ def delay_print(z)://self elxplain wanna put in go
         sys.stdout.write(t)//std.io
         sys.stdout.flush()//std.out i think 
         time.sleep(.05)//pause btw char r std.output 
+
+#try and think of a copy func right here
+
+class cpy:
+
+    def __init__(self):#gotta remember this is the args
+
+        self.Cpy = []
+        input() = n
+        
+        if n > 0:
+            self.Cpy[n++1]
+            return len(self.Cpy)#Think this copy and return
+        else:
+            return f''
