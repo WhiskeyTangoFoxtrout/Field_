bc02cf11f6ce85afbe4870bf8e32adccb069794f
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-02-11 16:48:57 -0600

    Update kern.py

--- kern.py
+++ kern.py
@@ -5,15 +5,17 @@ class egress:
 
     def __init__(self):
         name = Operation System
-        while True:
-            text = input('CmdPmt>')
-            print(text)
+        while True:#feel like this should be wit the curses module
+            indown(text = input('CmdPmt>'))
+            print(indown(text))
 
 
     def indown(self):#I gotta make a window and somehow have that while loop be my line where u ttype
         scr = curses.init()
-        curse.noecho()
-        curses.cbreak(False)
-        curses.def_shell_mode()
+        scr.refresh()
+        scr.noecho()
+        scr.cbreak(False)
+        scr.def_shell_mode()
+        
         
         
