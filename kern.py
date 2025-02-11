f9c3f4811089a59ac067833cf89807c4026e035b
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-02-11 14:39:25 -0600

    Update kern.py

--- kern.py
+++ kern.py
@@ -1,6 +1,19 @@
-import * from _Barnacle_buster
+import * from _Barnacle_buster, curses
 
 
-while True:
-    text = input('CmdPmt>')
-    print(text)
+class egress:
+
+    def __init__(self):
+        name = Operation System
+        while True:
+            text = input('CmdPmt>')
+            print(text)
+
+
+    def indown(self):#I gotta make a window and somehow have that while loop be my line where u ttype
+        scr = curses.init()
+        curse.noecho()
+        curses.cbreak(False)
+        curses.def_shell_mode()
+        
+        
