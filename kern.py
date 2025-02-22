c70644cff081670be4308602636f9eb934e130b9
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-02-22 13:44:44 -0600

    Update kern.py

--- kern.py
+++ kern.py
@@ -1,21 +1,34 @@
-import * from _Barnacle_buster, curses
+import * from _Barnacle_buster, curses, * from Rails.py
 
 
 class egress:
 
     def __init__(self):
-        name = Operation System
+        name == 'Operation System'
+
         while True:#feel like this should be wit the curses module
             indown(text = input('CmdPmt>'))
             print(indown(text))
 
 
-    def indown(self):#I gotta make a window and somehow have that while loop be my line where u ttype
-        scr = curses.init()
+    def indown(self):#mainwindow and bootup
+        scr = curses.init()#this is the whole screen
         scr.refresh()
         scr.noecho()
-        scr.cbreak(False)
+        scr.nocbreak()
         scr.def_shell_mode()
+        scr(movEprint("""This is a starter working on the whole kernal thing be on the look out for more updates this also is
+        the actual testing kernal so the fact you found this says a lot. Bless'd...also, where there is a compiler; updates, mainly "Dot E-X-E Files".
+        """))
+        if scr.beep() < 5:#ima have to check this
+            for scr.beep() in range(0,4):
+                curses.beep()
+            for scr.flash() in range(0,1):
+                curses.flash()
+        scr.refresh()
+        
+        scr.doupdate()
+        
         
         
         
